3006da9c3d0bbcad2aeeea8b6891615f1b8764bc
Akane <zarathustra47@yahoo.com>
2022-09-07 11:09:33 +0100

    categories are returned inside categories object

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,19 +8,19 @@ beforeEach(() => seed(testData));
 afterAll(() => db.end());
 
 describe('GET /api/categories', () => {
-    it('returns an array', () => {
+    it('returns a categories array', () => {
         return request(app).get('/api/categories')
         .expect(200)
         .then((res) => {
-            expect(Array.isArray(res.body)).toBe(true);
+            expect(Array.isArray(res.body.categories)).toBe(true);
         });
     });
     it('returns an array of categories with slug and description properties', () => {
         return request(app).get('/api/categories')
         .expect(200)
         .then((res) => {
-            expect(res.body[0].hasOwnProperty("slug")).toBe(true);
-            expect(res.body[0].hasOwnProperty("description")).toBe(true);
+            expect(res.body.categories[0].hasOwnProperty("slug")).toBe(true);
+            expect(res.body.categories[0].hasOwnProperty("description")).toBe(true);
         });
     });
 });
--- controllers/categories.controllers.js
+++ controllers/categories.controllers.js
@@ -3,7 +3,7 @@ const { selectCategories } = require('../models/categories.models');
 exports.getCategories = (req, res, next) => {
     selectCategories()
     .then((categories) => {
-        res.send(categories);
+        res.status(200).send({ categories });
     })
     .catch(next);
 };
