6ab50e4c3cd1224d2642d4d2dabf27122443d963
Akane <zarathustra47@yahoo.com>
2022-09-11 20:44:22 +0100

    adds comment_count to review

--- controllers/reviews.controllers.js
+++ controllers/reviews.controllers.js
@@ -1,9 +1,15 @@
-const { selectReview, updateReviewVotes } = require('../models/reviews.models');
+const { selectReview, countComments, updateReviewVotes } = require('../models/reviews.models');
 
 exports.getSingleReview = (req, res, next) => {
     const review_id = req.params;
-    selectReview(review_id)
+    let num = 0;
+    countComments(review_id)
+    .then((comment_count) => {
+        num = comment_count;
+        return selectReview(review_id)
+    })
     .then((review) => {
+        review.comment_count = num;
         res.status(200).send({ review });
     })
     .catch(next);
